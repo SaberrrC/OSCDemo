dc44a4378712d1b432872bc7bb2e6e1e3931b8bc
admin <admin>
2017-04-01 23:06:31 +0800

    更新1

---
+++ app/src/main/java/com/saberrr/openchina/bean/InformationHearBean.java
@@ -0,0 +1,15 @@
+package com.saberrr.openchina.bean;
+
+/**
+ * Created by 丁银晨 on 2017/4/1.
+ */
+
+public class InformationHearBean {
+    public int mImageView;
+    public String text;
+
+    public InformationHearBean(int imageView, String text) {
+        mImageView = imageView;
+        this.text = text;
+    }
+}
