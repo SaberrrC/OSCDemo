481d3e5dca2a9c40a2e20da39772479c1742f01f
SaberrrC <saberrrc@china.com>
2017-04-01 22:10:44 +0800

    初步添加网络框架

---
+++ app/src/main/java/com/saberrr/openchina/manager/netmanager/JsonCacheManager.java
@@ -0,0 +1,60 @@
+package com.saberrr.openchina.manager.netmanager;
+
+import android.text.TextUtils;
+
+import com.saberrr.openchina.utils.GsonTools;
+
+import java.util.List;
+
+/**
+ * Created by Saberrr on 2017-03-25.
+ */
+
+public class JsonCacheManager {
+
+    private static JsonCacheManager sJsonCacheManager = new JsonCacheManager();
+
+    private JsonCacheManager() {
+    }
+
+    public static synchronized JsonCacheManager getInstance() {
+        return sJsonCacheManager;
+    }
+
+    public <T> T getDataBean(String url, Class<T> bean) {
+        /**
+         * 1. 去网络请求最新数据
+         * 2. 如果没有数据去请求缓存数据
+         */
+        String json = NetMangager.getInstance().getJson(url);
+        if (TextUtils.isEmpty(json)) {
+            json = CacheManager.getInstance().getCacheData(url);
+        } else {
+            CacheManager.getInstance().saveCacheData(url, json);
+        }
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        } else {
+            return GsonTools.changeGsonToBean(json, bean);
+        }
+    }
+
+    public <T> List<T> getDataList(String url, Class<T> t) {
+        /**
+         * 1. 去网络请求最新数据
+         * 2. 如果没有数据去请求缓存数据
+         */
+        String json = NetMangager.getInstance().getJson(url);
+        if (TextUtils.isEmpty(json)) {
+            json = CacheManager.getInstance().getCacheData(url);
+        } else {
+            CacheManager.getInstance().saveCacheData(url, json);
+        }
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        } else {
+            //            return GsonUtil.fromJsonArray(json, t);
+            return GsonTools.changeGsonToList(json, t);
+        }
+    }
+}
