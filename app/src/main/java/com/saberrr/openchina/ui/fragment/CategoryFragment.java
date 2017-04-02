c5da5ee483d3184584b08b568d841570c8219e82
liuqi <http://admin@liuqi-pc:10101/r/Test19.git>
2017-04-02 23:58:19 +0800

    获取数据成功3

--- app/src/main/java/com/saberrr/openchina/ui/fragment/CategoryFragment.java
+++ app/src/main/java/com/saberrr/openchina/ui/fragment/CategoryFragment.java
@@ -10,8 +10,11 @@ import com.saberrr.openchina.R;
 import com.saberrr.openchina.bean.CategoryBean;
 import com.saberrr.openchina.bean.SoftwareType;
 import com.saberrr.openchina.net.Urls;
+import com.saberrr.openchina.ui.activity.ShowActivity;
+import com.saberrr.openchina.ui.adapter.CategoryAdapter;
 import com.saberrr.openchina.ui.adapter.FinalRecycleAdapter;
 import com.saberrr.openchina.utils.ThreadUtils;
+import com.saberrr.openchina.utils.ToastUtils;
 import com.saberrr.openchina.utils.XmlUtils;
 
 import java.io.IOException;
@@ -34,10 +37,11 @@ public class CategoryFragment extends BaseFragment implements FinalRecycleAdapte
 
     @BindView(R.id.category_recyclerview)
     RecyclerView mCategoryRecyclerview;
-    private List<String> datas = new ArrayList<>();
+    private List<String> names = new ArrayList<>();
+    private List<Integer> tags = new ArrayList<>();
     private HashMap<Class, Integer> classIntegerHashMap = new HashMap<>();
     private List<SoftwareType> mSoftwareTypes;
-    private FinalRecycleAdapter mFinalRecycleAdapter;
+    private CategoryAdapter mFinalRecycleAdapter;
 
     @Override
     protected boolean needRefresh() {
@@ -53,10 +57,51 @@ public class CategoryFragment extends BaseFragment implements FinalRecycleAdapte
     }
 
     private void init() {
-        classIntegerHashMap.put(String.class,R.layout.category_item_layout);
-        mFinalRecycleAdapter = new FinalRecycleAdapter(datas, classIntegerHashMap, this);
+        /*classIntegerHashMap.put(String.class,R.layout.category_item_layout);
+        mFinalRecycleAdapter = new FinalRecycleAdapter(datas, classIntegerHashMap, this);*/
+        mFinalRecycleAdapter = new CategoryAdapter(names);
         mCategoryRecyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
         mCategoryRecyclerview.setAdapter(mFinalRecycleAdapter);
+        mFinalRecycleAdapter.setOnCategoryItemClickListener(new CategoryAdapter.OnCategoryItemClickListener() {
+            @Override
+            public void onCategoryItemClick(final int position) {
+                ToastUtils.showToast(position+"");
+                names.clear();
+
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        OkHttpClient okHttpClient = new OkHttpClient();
+                        Request request = new Request.Builder().url(Urls.DETAIL+tags.get(position)).build();
+                        try {
+                            Response response = okHttpClient.newCall(request).execute();
+                            String xml = response.body().string();
+                            System.out.println(xml);
+                            CategoryBean categoryBean = XmlUtils.toBean(CategoryBean.class, xml.getBytes());
+                            List<SoftwareType> mSoftwareTypes = categoryBean.getSoftwareTypes();
+                            for (int i = 0; i < mSoftwareTypes.size(); i++) {
+                                String name = mSoftwareTypes.get(i).name;
+                                names.add(name);
+                                System.out.println(name);
+                            }
+                            ThreadUtils.runMain(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mFinalRecycleAdapter.notifyDataSetChanged();
+                                }
+                            });
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+
+
+
+            }
+        });
     }
 
     @Override
@@ -69,10 +114,12 @@ public class CategoryFragment extends BaseFragment implements FinalRecycleAdapte
                     String xml = response.body().string();
                     System.out.println(xml);
                     CategoryBean categoryBean = XmlUtils.toBean(CategoryBean.class, xml.getBytes());
-                    mSoftwareTypes = categoryBean.getSoftwareTypes();
+                    List<SoftwareType> mSoftwareTypes = categoryBean.getSoftwareTypes();
                     for (int i = 0; i < mSoftwareTypes.size(); i++) {
                         String name = mSoftwareTypes.get(i).name;
-                        datas.add(name);
+                        int tag = mSoftwareTypes.get(i).tag;
+                        tags.add(tag);
+                        names.add(name);
                         System.out.println(name);
                     }
                 } catch (IOException e) {
@@ -84,12 +131,11 @@ public class CategoryFragment extends BaseFragment implements FinalRecycleAdapte
         ThreadUtils.runMain(new Runnable() {
             @Override
             public void run() {
-                mCategoryRecyclerview.requestLayout();
                 mFinalRecycleAdapter.notifyDataSetChanged();
             }
         });
 
-        return mSoftwareTypes;
+        return "";
     }
 
 
