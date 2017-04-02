3dcbdbcdbbaffc26dd2eb11f66c5c16515d66978
dingyinchen <admin>
2017-04-02 23:07:45 +0800

    更新11111

---
+++ app/src/main/java/com/saberrr/openchina/utils/Constant.java
@@ -0,0 +1,12 @@
+package com.saberrr.openchina.utils;
+
+/**
+ * Created by 丁银晨 on 2017/4/2.
+ */
+
+public class Constant {
+    public static class BLOGDETAILSFRAGMENT {
+        public static final String HREF      = "HREF";
+        public static final String COMMENTCOUNT = "COMMENTCOUNT";
+    }
+}
