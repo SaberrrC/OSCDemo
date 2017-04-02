9966bb5909a172088cbf4f207a5ade73aa69b490
liuqi <http://admin@liuqi-pc:10101/r/Test19.git>
2017-04-03 02:02:37 +0800

    获取所有地址

--- app/src/main/java/com/saberrr/openchina/net/Urls.java
+++ app/src/main/java/com/saberrr/openchina/net/Urls.java
@@ -12,6 +12,10 @@ public class Urls {
     public static String MOVE_NEW = BASE_URL + "action/apiv2/tweets?type=1";
     public static final String CATEGORY = BASE_URL + "action/api/softwarecatalog_list?tag=0";
     public static final String DETAIL = BASE_URL + "action/api/softwarecatalog_list?tag=";
+    public static final String RECOMMEND = BASE_URL + "action/api/software_list?pageIndex=0&searchTag=recommend&pageSize=20";
+    public static final String BESTNEW = BASE_URL + "action/api/software_list?pageIndex=0&searchTag=time&pageSize=20";
+    public static final String HOT = BASE_URL + "action/api/software_list?pageIndex=0&searchTag=view&pageSize=20";
+    public static final String DOMESTIC = BASE_URL + "action/api/software_list?pageIndex=0&searchTag=list_cn&pageSize=20";
 
 
 }
