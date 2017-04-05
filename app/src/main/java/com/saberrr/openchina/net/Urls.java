66d5c7a5fee3799efea46a8bbaf63c283ffc2139
chumingyuan <chumingyuan>
2017-04-05 23:52:55 +0800

    弹一弹界面展示完成

--- app/src/main/java/com/saberrr/openchina/net/Urls.java
+++ app/src/main/java/com/saberrr/openchina/net/Urls.java
@@ -31,5 +31,7 @@ public class Urls {
     public static  final String TWEETLIKE = "/action/api/my_tweet_like_list";
     //发表动弹
     public static final String SEND_JUMP = BASE_URL + "action/apiv2/";
+    public static final String SEND_JUMP_TEXT = BASE_URL + "action/apiv2/tweet";
+
 
 }
