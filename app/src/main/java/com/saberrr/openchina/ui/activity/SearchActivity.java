9360a8321613ec998397fc1b9ccb4eca7af87a70
liuqi <http://admin@liuqi-pc:10101/r/Test19.git>
2017-04-08 19:56:38 +0800

    搜索添加沉浸式状态栏

--- app/src/main/java/com/saberrr/openchina/ui/activity/SearchActivity.java
+++ app/src/main/java/com/saberrr/openchina/ui/activity/SearchActivity.java
@@ -2,6 +2,7 @@ package com.saberrr.openchina.ui.activity;
 
 import android.content.DialogInterface;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -12,6 +13,8 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -65,6 +68,22 @@ public class SearchActivity extends AppCompatActivity implements TextWatcher, Ke
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        // 这个属性4.4算是全透明（有的机子是过渡形式的透明），5.0就是半透明了 我的模拟器、真机都是半透明，
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+        {// 4.4 全透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {// 5.0 全透明实现
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);// calculateStatusColor(Color.WHITE, (int) alphaValue)
+        }
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
         init();
